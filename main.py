c7139ff682d1ba1d5edab697a553608ec01b82ed
Paul Smith <psmith@magna.co.uk>
2024-05-29 11:05:17 +0100

    Main functionality

---
+++ main.py
@@ -0,0 +1,36 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    
+    print (f"--- Begin report of {book_path} ---")
+    print (f"{num_words} words found in the document")
+    sorted_dict = dict(sorted(chars_dict.items(), key=lambda item: item[1], reverse=True))
+    for c in sorted_dict:
+        if c.isalpha():
+            print(f"The '{c}' character was found {chars_dict[c]} times")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
